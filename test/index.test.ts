8d7baaa7a0727a7e234181b239e3270a9bdb55e9
Kaho Cheung <kaho@elefanttraks.com>
2020-05-06 20:58:24 +1000

    Add tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -19,19 +19,78 @@ describe('Render', () => {
     expect(render('.test', [])).toEqual({ _groups: [], _parents: [] });
   });
 
-  it('should render svg', () => {
-    const svg = getExampleDOM();
+  it('should render text element in svg with multiple attributes', () => {
+    const svg = getExampleDOM('svg');
 
-    render(svg, [
+    const data = [
       {
         append: 'text',
         text: 'test',
         x: 5,
+        y: 5,
+        onClick: () => {},
       },
-    ]);
+    ];
 
-    expect(getByText(svg, 'test')).toBeTruthy();
+    render(svg, data);
+
+    expect(svg.nodeName).toEqual('SVG');
     expect(svg.querySelector('text')).toBeTruthy();
-    console.log(svg.querySelector('text').getAttribute('x'));
+    expect(getByText(svg, 'test')).toBeTruthy();
+
+    data.forEach(d => {
+      Object.keys(d).map(key => {
+        const keyType = getKeyType(key);
+
+        if (keyType === 'attribute') {
+          const realValue = d[key];
+          const testValue = svg.querySelector('text').getAttribute(key);
+
+          expect(testValue).toEqual(realValue.toString());
+        }
+      });
+    });
+  });
+
+  it("should render rect of class 'rect1' with inline styles", () => {
+    const svg = getExampleDOM('svg');
+
+    const data = [
+      {
+        append: 'rect',
+        class: 'rect1',
+        fill: 'red',
+        style: {
+          stroke: 'black',
+          fillOpacity: 0.5,
+        },
+      },
+    ];
+
+    render(svg, data);
+
+    const rect = svg.querySelector('.rect1');
+
+    expect(rect.getAttribute('style')).toEqual(
+      'stroke: black; fill-opacity: 0.5;'
+    );
   });
 });
+
+// TODO:
+// Test children and nesting
+// Test id
+// Test onClick etc
+// Test styles
+// Test { enter, exit } transition object
+// Test function attribute value
+
+function getKeyType(key: string) {
+  if (['append', 'text', 'style'].includes(key)) {
+    return key;
+  } else if (key.startsWith('on')) {
+    return 'handler';
+  }
+
+  return 'attribute';
+}
