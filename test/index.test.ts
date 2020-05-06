aa056c377115f44b056652d8996c2368641ccf39
Kaho Cheung <kaho@elefanttraks.com>
2020-05-07 00:28:21 +1000

    Add more tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,6 +6,7 @@ import {
   // Tip: all queries are also exposed on an object
   // called "queries" which you could import here as well
   // waitFor,
+  fireEvent,
 } from '@testing-library/dom';
 
 import render from '../src';
@@ -21,7 +22,6 @@ describe('Render', () => {
 
   it('should render text element in svg with multiple attributes', () => {
     const svg = getExampleDOM('svg');
-
     const data = [
       {
         append: 'text',
@@ -54,7 +54,6 @@ describe('Render', () => {
 
   it("should render rect of class 'rect1' with inline styles", () => {
     const svg = getExampleDOM('svg');
-
     const data = [
       {
         append: 'rect',
@@ -75,15 +74,95 @@ describe('Render', () => {
       'stroke: black; fill-opacity: 0.5;'
     );
   });
-});
 
-// TODO:
-// Test children and nesting
-// Test id
-// Test onClick etc
-// Test styles
-// Test { enter, exit } transition object
-// Test function attribute value
+  it("should render group of id '#group1' with children", () => {
+    const svg = getExampleDOM('svg');
+    const data = [
+      {
+        append: 'g',
+        id: 'group1',
+        children: [
+          {
+            append: 'circle',
+            r: 10,
+          },
+        ],
+      },
+    ];
+
+    render(svg, data);
+
+    const group = svg.querySelector('#group1');
+    const circle = group.querySelector('circle');
+
+    expect(circle).toBeTruthy();
+    expect(circle.getAttribute('r')).toEqual('10');
+  });
+
+  it('should render rect with initial width from transition object', () => {
+    const svg = getExampleDOM('svg');
+
+    const data = [
+      {
+        append: 'rect',
+        width: { enter: 100, exit: 0 },
+      },
+    ];
+
+    render(svg, data);
+
+    const rect = svg.querySelector('rect');
+    expect(rect.getAttribute('width')).toEqual('0');
+  });
+
+  it('should render rect with an onClick handler', () => {
+    const svg = getExampleDOM('svg');
+    const onClick = jest.fn();
+    const data = [
+      {
+        append: 'rect',
+        width: 100,
+        height: 100,
+        onClick,
+      },
+    ];
+
+    render(svg, data);
+
+    const rect = svg.querySelector('rect');
+    fireEvent(rect, new MouseEvent('click'));
+    expect(onClick).toBeCalledWith(
+      expect.objectContaining({
+        isTrusted: false,
+      }),
+      expect.objectContaining({
+        append: 'rect',
+      }),
+      0
+    );
+  });
+
+  it('should render ellipse with a function attribute value', () => {
+    const svg = getExampleDOM('svg');
+    const rxMock = jest.fn();
+    const data = [
+      {
+        append: 'ellipse',
+        rx: (d, i) => {
+          rxMock(d, i);
+          return 100;
+        },
+        ry: 50,
+      },
+    ];
+
+    render(svg, data);
+
+    const ellipse = svg.querySelector('ellipse');
+    expect(ellipse.getAttribute('rx')).toEqual('100');
+    expect(rxMock).toBeCalledWith(data[0], 0);
+  });
+});
 
 function getKeyType(key: string) {
   if (['append', 'text', 'style'].includes(key)) {
